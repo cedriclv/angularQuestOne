2fc6d25e331cfd36bac418690ab0855bf49e26d6
CEDRIC LV <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-02-18 16:42:34 +0100

    quete5Fin

--- src/app/shared/models/User.ts
+++ src/app/shared/models/User.ts
@@ -1,27 +1,8 @@
 export class User {
-  public lastName: string = 'Le Valegant';
-  public firstName: string = 'Cedric';
-  public age?: number = 44;
-  public quote?: string = 'I neeed a break';
-  public photo?: string = 'https://randomuser.me/api/portraits/lego/2.jpg';
+  public lastName?: string;
+  public firstName?: string;
   public email?: string;
   public password?: string;
-
-  constructor(
-    lastName: string,
-    firstName: string,
-    age: number,
-    quote: string,
-    photo: string,
-    email: string = 'toto@toto.com',
-    password : string ='toto'
-  ) {
-    this.lastName = lastName;
-    this.firstName = firstName;
-    this.age = age;
-    this.quote = quote;
-    this.photo = photo;
-    this.email = email;
-    this.password = password;
-  }
+  public age?: number;
+  public quote?: string;
 }
